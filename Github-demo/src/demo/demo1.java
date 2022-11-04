56a159f6aeda48b34fa97f60d126db336d2381b5
Szy <116250487+ACQYQ@users.noreply.github.com>
2022-11-04 11:44:29 +0800

    Update demo1.java

--- Github-demo/src/demo/demo1.java
+++ Github-demo/src/demo/demo1.java
@@ -4,5 +4,6 @@ public class demo1 {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
